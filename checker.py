948fa7954c22542ecaa467c889e6fa24d0b315ac
Vasilije Marković <markovic.sv15.2021@uns.ac.rs>
2024-06-25 14:57:41 +0200

    Implemented RNN language model

--- checker.py
+++ checker.py
@@ -1,7 +1,11 @@
 import string
+from utils import *
 
 TEXTS_FILE_NAME = "texts.txt"
 DICT_FILE_NAME = "dict.txt"
+
+EMBEDDING_DIM = 64
+HIDDEN_DIM = 128
 
 if __name__ == "__main__":
     sentences = []
@@ -21,6 +25,22 @@ if __name__ == "__main__":
     with open(DICT_FILE_NAME, 'r', encoding='utf-8') as file:
         for line in file:
             dictionary.append(line)
+    
+    tokenizer = Tokenizer()
+    tokenizer.fit(sentences)
 
-    print(len(dictionary))
-    print(len(sentences))
+    encoded_sentences = [torch.tensor(tokenizer.encode(sentence)) for sentence in sentences]
+    padded_sentences = pad_sequence(encoded_sentences, batch_first=True, padding_value=tokenizer.word2idx['<PAD>'])
+
+    dataset = SentenceDataset(padded_sentences)
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+    
+    vocab_size = tokenizer.vocab_size
+
+    model = LanguageModel(vocab_size, EMBEDDING_DIM, HIDDEN_DIM)
+    criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.word2idx['<PAD>'])
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    
+    corrected_sentence, best_mask_position = correct_sentence_mask(model, tokenizer, "Iz danas je amognus iz", sentences)
+    print(f"Corrected: {corrected_sentence} (Best mask position: {best_mask_position})")
+
--- utils.py
+++ utils.py
@@ -39,5 +39,73 @@ class Tokenizer:
     def insert(self, tokens, position, token):
         return tokens[:position] + [token] + tokens[position:]
 
+class SentenceDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
 
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx][:-1], self.data[idx][1:]
+
+class LanguageModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
+        super(LanguageModel, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, vocab_size)
+
+    def forward(self, x):
+        embedded = self.embedding(x)
+        lstm_out, _ = self.lstm(embedded)
+        output = self.fc(lstm_out)
+        return output
+
+def correct_sentence_mask(model, tokenizer, input_sentence, sentences):
+    model.eval()
+    best_corrected_sentence = input_sentence
+    best_accuracy = 0.0
+    best_mask_position = -1
+
+    for mask_position in range(len(input_sentence.split()) + 1):
+        with torch.no_grad():
+            tokens = input_sentence.split()
+            tokens.insert(mask_position, tokenizer.mask_token)
+            input_ids = tokenizer.convert_tokens_to_ids(tokens)
+            input_seq = torch.tensor([input_ids])
+
+            output = model(input_seq)
+            masked_index = input_ids.index(tokenizer.word2idx[tokenizer.mask_token])
+            masked_token_logits = output[0, masked_index, :]
+
+            predicted_token_id = torch.argmax(masked_token_logits, dim=-1).item()
+            predicted_token = tokenizer.idx2word[predicted_token_id]
+
+            tokens[masked_index] = predicted_token
+
+            corrected_sentence = tokenizer.convert_tokens_to_string(tokens)
+
+            for sentence in sentences:
+                accuracy = calculate_accuracy(corrected_sentence, sentence)
+                if accuracy > best_accuracy:
+                   best_accuracy = accuracy
+                   best_corrected_sentence = corrected_sentence
+                   best_mask_position = mask_position
+
+    return best_corrected_sentence, best_mask_position
+
+
+def calculate_accuracy(corrected_sentence, original_sentence):
+    corrected_tokens = corrected_sentence.split()
+    original_tokens = original_sentence.split()
+
+    distance = levenshtein_distance(corrected_tokens, original_tokens)
+
+    max_length = max(len(corrected_tokens), len(original_tokens))
+    if max_length == 0:
+        return 0.0
+    accuracy = 1 - (distance / max_length)
+
+    return accuracy
 
