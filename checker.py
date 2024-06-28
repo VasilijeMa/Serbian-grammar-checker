220b639d5109ae7fe68ad2c3226ea639fe9df283
Vasilije Marković <markovic.sv15.2021@uns.ac.rs>
2024-06-28 13:43:24 +0200

    Changed model to observe words between other words

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 import string
 from utils import *
+from collections import Counter
 
 TEXTS_FILE_NAME = "texts.txt"
 VOCAB_FILE_NAME = "dict.txt"
@@ -14,43 +15,72 @@ if __name__ == "__main__":
     vocabulary = []
 
     with open(TEXTS_FILE_NAME, 'r', encoding='utf-8') as file:
+        i = 0
         for line in file:
+            i+=1
             lines = line.split("\t")
             sentences.append(preprocess_sentence(lines[1].strip(), LETTERS))
             sentences.append(preprocess_sentence(lines[2].strip(), LETTERS))
+            if i==100: break
 
     with open(VOCAB_FILE_NAME, 'r', encoding='utf-8') as file:
         for line in file:
             vocabulary.append(line.strip())
-    
-    tokenizer = Tokenizer()
-    tokenizer.fit(sentences)
 
-    encoded_sentences = [torch.tensor(tokenizer.encode(sentence)) for sentence in sentences]
-    padded_sentences = pad_sequence(encoded_sentences, batch_first=True, padding_value=tokenizer.word2idx['<PAD>'])
+    tokenized_sentences = [sentence.split() for sentence in sentences]
+    unique_words = set(word for sentence in tokenized_sentences for word in sentence)
 
-    dataset = SentenceDataset(padded_sentences)
-    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
-    
-    vocab_size = tokenizer.vocab_size
+    vocab = {word: i+1 for i, word in enumerate(unique_words)}
+    inv_vocab = {v: k for k, v in vocab.items()}
 
-    model = LanguageModel(vocab_size, EMBEDDING_DIM, HIDDEN_DIM)
-    criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.word2idx['<PAD>'])
+    sequences = [[vocab[word] for word in sentence] for sentence in tokenized_sentences]
+
+    def create_io_pairs(sequences):
+        X_before, X_after, y = [], [], []
+        for sequence in sequences:
+            if len(sequence) > 2:
+                for i in range(1, len(sequence)-1):
+                    X_before.append(sequence[i-1])
+                    X_after.append(sequence[i+1])
+                    y.append(sequence[i])
+        return X_before, X_after, y
+
+    X_before, X_after, y = create_io_pairs(sequences)
+
+    X_before = torch.tensor(X_before, dtype=torch.long).unsqueeze(1)
+    X_after = torch.tensor(X_after, dtype=torch.long).unsqueeze(1)
+    y = torch.tensor(y, dtype=torch.long)
+
+    dataset = WordDataset(X_before, X_after, y)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+    vocab_size = len(vocab) + 1
+    model = WordBetweenModel(vocab_size, EMBEDDING_DIM, HIDDEN_DIM)
+
+    criterion = nn.CrossEntropyLoss()
+
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     for epoch in range(NUM_EPOCHS):
-        for inputs, targets in dataloader:
-            outputs = model(inputs)
-            loss = criterion(outputs.view(-1, vocab_size), targets.view(-1))
+        print(f"Starting epoch {epoch+1}")
+        i = 1
+        for input_before, input_after, target in dataloader:
+            input_before = input_before.unsqueeze(1)
+            input_after = input_after.unsqueeze(1)
+            
+            outputs = model(input_before, input_after)
+            loss = criterion(outputs, target)
             optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        print(f'Epoch {epoch + 1}/{NUM_EPOCHS}, Loss: {loss.item()}')
 
-    # prompt = preprocess_sentence("Ovo je rekenica", LETTERS)
-    # for combination in spellcheck_sentence(prompt, vocabulary):
-    #     print(' '.join(word for word in combination))
+            loss.backward()
+
+            optimizer.step()
+            
+        print(f'Epoch [{epoch+1}/{NUM_EPOCHS}], Loss: {loss.item():.4f}')
+
+    predicted_word = predict_between(model, "nalazi", "centru", vocab)
+    print(predicted_word)
     
-    corrected_sentence, best_mask_position = correct_sentence_mask(model, tokenizer, "euleks je u pristini", sentences)
-    print(f"Corrected: {corrected_sentence} (Best mask position: {best_mask_position})")
+    
+
 
--- utils.py
+++ utils.py
@@ -6,99 +6,35 @@ from torch.nn.utils.rnn import pad_sequence
 from Levenshtein import distance as levenshtein_distance
 import itertools
 
-class Tokenizer:
-    def __init__(self):
-        self.word2idx = {}
-        self.idx2word = {}
-        self.vocab_size = 0
-        self.mask_token = '<MASK>'
-
-    def fit(self, sentences):
-        unique_words = set(word for sentence in sentences for word in sentence.split())
-        self.vocab_size = len(unique_words) + 3  # Add 3 for <PAD>, <EOS>, and <MASK>
-        self.word2idx = {word: idx + 1 for idx, word in enumerate(unique_words)}
-        self.word2idx['<PAD>'] = 0
-        self.word2idx['<EOS>'] = self.vocab_size - 2
-        self.word2idx[self.mask_token] = self.vocab_size - 1
-        self.idx2word = {idx: word for word, idx in self.word2idx.items()}
-
-    def encode(self, sentence):
-        return [self.word2idx[word] for word in sentence.split()] + [self.word2idx['<EOS>']]
-
-    def decode(self, indices):
-        return ' '.join(self.idx2word[idx] for idx in indices if idx not in (0, self.word2idx['<EOS>']))
-
-    def convert_tokens_to_ids(self, tokens):
-        return [self.word2idx.get(token, self.word2idx['<PAD>']) for token in tokens]
-
-    def convert_ids_to_tokens(self, ids):
-        return [self.idx2word[id] for id in ids]
-
-    def convert_tokens_to_string(self, tokens):
-        return ' '.join(tokens)
-
-    def insert(self, tokens, position, token):
-        return tokens[:position] + [token] + tokens[position:]
-
-class SentenceDataset(Dataset):
-    def __init__(self, data):
-        self.data = data
+class WordDataset(Dataset):
+    def __init__(self, X_before, X_after, y):
+        self.X_before = X_before
+        self.X_after = X_after
+        self.y = y
 
     def __len__(self):
-        return len(self.data)
+        return len(self.y)
 
     def __getitem__(self, idx):
-        return self.data[idx][:-1], self.data[idx][1:]
+        return self.X_before[idx], self.X_after[idx], self.y[idx]
 
-class LanguageModel(nn.Module):
+class WordBetweenModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim):
-        super(LanguageModel, self).__init__()
+        super(WordBetweenModel, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
         self.fc = nn.Linear(hidden_dim, vocab_size)
 
-    def forward(self, x):
-        embedded = self.embedding(x)
-        lstm_out, _ = self.lstm(embedded)
-        output = self.fc(lstm_out)
-        return output
-
-def correct_sentence_mask(model, tokenizer, input_sentence, sentences):
-    model.eval()
-    best_corrected_sentence = input_sentence
-    best_accuracy = 0.0
-    best_mask_position = -1
-
-    for mask_position in range(len(input_sentence.split()) + 1):
-        with torch.no_grad():
-            tokens = input_sentence.split()
-            tokens.insert(mask_position, tokenizer.mask_token)
-            input_ids = tokenizer.convert_tokens_to_ids(tokens)
-            input_seq = torch.tensor([input_ids])
-
-            output = model(input_seq)
-            masked_index = input_ids.index(tokenizer.word2idx[tokenizer.mask_token])
-            masked_token_logits = output[0, masked_index, :]
-
-            predicted_token_id = torch.argmax(masked_token_logits, dim=-1).item()
-            predicted_token = tokenizer.idx2word[predicted_token_id]
-
-            tokens[masked_index] = predicted_token
-
-            corrected_sentence = tokenizer.convert_tokens_to_string(tokens)
-
-            for sentence in sentences:
-                accuracy = calculate_accuracy(corrected_sentence, sentence)
-                if accuracy > best_accuracy:
-                   best_accuracy = accuracy
-                   best_corrected_sentence = corrected_sentence
-                   best_mask_position = mask_position
-
-    return best_corrected_sentence, best_mask_position
-
-
-#def correct_sentence_replace(model, tokenizer, input_sentence, sentences):
-
+    def forward(self, x_before, x_after):
+        embed_before = self.embedding(x_before)
+        embed_after = self.embedding(x_after)
+        
+        combined = torch.cat((embed_before, embed_after), dim=2)
+        combined = combined.squeeze(1)
+        lstm_out, _ = self.lstm(combined)
+        
+        out = self.fc(lstm_out[:, -1, :])
+        return out
 
 def calculate_accuracy(corrected_sentence, original_sentence):
     corrected_tokens = corrected_sentence.split()
@@ -128,7 +64,7 @@ def spellcheck_word(given_word, vocabulary):
     return nearest_words
 
 def preprocess_sentence(sentence, letters):
-    return ' '.join(''.join(letter for letter in sentence if letter in letters).split())
+    return ' '.join(''.join(letter for letter in sentence if letter in letters).split()) + " <EOS>"
 
 def spellcheck_sentence(sentence, vocabulary):
     original_words = sentence.split()
@@ -149,6 +85,19 @@ def spellcheck_sentence(sentence, vocabulary):
     return spellchecked_sentences
 
 
-
-
-
+def predict_between(model, context_before, context_after, vocab):
+    model.eval()
+    
+    indices_before = [vocab[context_before]]
+    indices_after = [vocab[context_after]]
+    
+    input_before = torch.tensor([indices_before], dtype=torch.long).unsqueeze(1)
+    input_after = torch.tensor([indices_after], dtype=torch.long).unsqueeze(1)
+    
+    with torch.no_grad():
+        output = model(input_before, input_after)
+        _, predicted = torch.max(output, 1)
+        predicted_word = predicted.item()
+    
+    inv_vocab = {i: word for word, i in vocab.items()}
+    return ' '.join([context_before, inv_vocab[predicted_word], context_after])
